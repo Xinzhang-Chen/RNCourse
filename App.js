57b0bb7b0ac6fafca1ce76c239990b45f9ceea91
Xinzhang-Chen <xinzhang.chen@yahoo.com>
2022-12-04 13:10:54 +1100

    Style the UI

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const goalInputHandler = (inputText) => {
+    console.log(inputText);
+  };
+
+  const addGoalHandler = () => {};
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputField} placeholder="please input your goal" />
+        <TextInput style={styles.inputField} placeholder="please input your goal" onChangeText={goalInputHandler} />
         <Button title="add goal" />
       </View>
-      <View>
+
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -16,17 +23,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flexDirection: 'column',
+    flex: 1,
   },
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    flex: 1,
   },
   inputField: {
-    width: '80%',
+    width: '70%',
     borderWidth: 1,
     borderColor: '#cccccc',
     marginRight: 8,
     padding: 3,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
